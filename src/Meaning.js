2626869d51425cc938c335d057d97b65072897d8
Irene Bellani <bellani.irene@gmail.com>
2023-09-30 17:05:29 +0200

    Styled meanings, examples, synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Example from "./Example.js";
 import Synonyms from "./Synonyms.js";
-import "./Meanings.css";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
     <div>
-      <h4>{props.meaning.partOfSpeech}</h4>
-      <p>{props.meaning.definition}</p>
-      <p>
+      <h4 className="Part-of-speech">{props.meaning.partOfSpeech}</h4>
+      <p className="Meaning">{props.meaning.definition}</p>
+      <p className="Example">
         <Example example={props.meaning.example} />
       </p>
-      <p>
+      <p className="Synonym">
         <Synonyms synonyms={props.meaning.synonyms} />
       </p>
     </div>
